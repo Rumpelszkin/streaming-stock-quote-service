81895514ef94813c9253389a042b3a6b1c235b6d
Rumpelszkin <kolbuszewskimax@gmail.com>
2021-08-09 12:10:44 +0200

    init commit

---
+++ src/test/java/com/rmp/streamingstockquoteservice/StreamingStockQuoteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rmp.streamingstockquoteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamingStockQuoteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
